1214339e6c652280018a88f8096a8cb1fd4fd89a
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-09-22 18:21:05 -0700

    Add files via upload

---
+++ howToughAreYou.js
@@ -0,0 +1,16 @@
+var tuffLevel = 2;
+if(tuffLevel == 10){
+    console.log("Woah, what did your mom feed you man... uhh, you may enter");
+}
+else if(tuffLevel >= 8){
+    console.log("You may enter at your own risk");
+}
+else{
+    console.log("Not tough enough, maybe next life son");
+}
+
+
+
+
+
+
