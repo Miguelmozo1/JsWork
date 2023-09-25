ce6d15804c84d074e1ddf359a8bab1aa3ffc15c9
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-09-25 15:56:09 -0700

    Add files via upload

---
+++ 21orOlder?.js
@@ -0,0 +1,13 @@
+var age = 21;
+let nameOnID = "Miguel Mozo"
+console.log("Let me some ID please");
+console.log("Hmm");
+if(age >= 21){
+
+    if(nameOnID == "Miguel Mozo"){
+    console.log("Thank you, welcome to Boomers");
+    }
+}    
+else{
+    console.log("Mini golf is that way");
+}
