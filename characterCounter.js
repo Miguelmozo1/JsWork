ac0a133a7f36da6633a797f98679294160ba0b6e
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-09-26 16:09:35 -0700

    Add files via upload

---
+++ characterCounter.js
@@ -0,0 +1,5 @@
+function wordCounter(string){
+string = string.length
+    return string;
+}
+console.log("There are", wordCounter("How many people don't like pumpkin pie?"), "characters in your string, including spaces haha");
