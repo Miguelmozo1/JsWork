bcc131373a2cfebe02fc84c186e306bd39dbf039
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-10-28 20:11:02 -0700

    Add files via upload

---
+++ getToKnowYou.rb
@@ -0,0 +1,9 @@
+puts("What is your first name?")
+first_name = gets.chomp
+puts("Thanks, your last name?")
+last_name = gets.chomp
+puts("Nice name, what about the city you grew up in?")
+cHood_city = gets.chomp
+puts("Hmmm, never heard of it, what state is it in?")
+state = gets.chomp
+puts("Gotcha, #{first_name} #{last_name}, and you're from #{cHood_city}, #{state}. Pleasure to meet you")
