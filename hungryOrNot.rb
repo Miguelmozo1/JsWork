2e100731937574fe2780cc5a202d4eea29ec3eca
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-10-29 21:32:20 -0700

    Add files via upload

---
+++ hungryOrNot.rb
@@ -0,0 +1,6 @@
+hungry = true               # when true, "unless" will be skipped over since argument is true. Only works when condition is false
+unless hungry
+    puts("Could knock off a couple more things")
+else
+    puts("Time for something to feed me and the brain")
+end
