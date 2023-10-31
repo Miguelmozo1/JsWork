5e8c504ab37a20450d843765ac8fce93b6db4c1d
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-10-30 18:38:16 -0700

    Add files via upload

---
+++ simpleIntro.rb
@@ -0,0 +1,9 @@
+my_name = "Miguel"
+puts(my_name)
+hobbies = ["guitar", "hiking", "camping", "coding", "reading", "snowboarding"]
+# updating my name to include last name
+puts("full name?")
+my_name = gets.chomp
+puts("Full Name: " + my_name)
+puts("Oh, I know you, you like:")
+puts(hobbies)
