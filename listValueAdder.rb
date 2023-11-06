735b43ce5f65aa004e336d7344ba96dac0073008
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-11-05 20:25:22 -0800

    Add files via upload

---
+++ listValueAdder.rb
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5]
+arr.each do |x|             # each is a method to iterate through. Its like a one-time loop
+    x += 10
+    puts("#{x}")
+end                         # since we figured out that each is a loop
+
+
