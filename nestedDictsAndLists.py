3b9f59809b59bef3dcb9c9670128fae83e0c63ed
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-11-13 20:50:18 -0800

    Add files via upload

---
+++ nestedDictsAndLists.py
@@ -0,0 +1,17 @@
+
+
+guitar_brands_and_info = [
+    {"Brand": "Yamaha", "Founded": "1887", "Founder": "Torakusu Yamaha"},
+    {"Brand": "Gibson", "Founded": "1902", "Founder": "Orville Gibson"},
+    {"Brand": "Fender", "Founded": "1946", "Founder": "Leo Fender"}
+]
+
+# nested list in a dictionary
+
+books_read_recently = {
+    "Non-Fiction": ["Guns, Germs, and Steel", "The Big Short", "To Engineer is to human"],
+    "Scince": ["The Disappering Spoon", "The Fabric of the Cosmos", "In Search of Schrodinger's Cat"],
+    "Religion": ["Bhagavad Gita"],
+    "History": ["The Crusaders", "Vikings", "The Island at the Center of the World"]
+}
+
