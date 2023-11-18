01f9f851bae43f5b07541c7bda5a5eaf8e635ec2
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-11-18 09:56:10 -0800

    Add files via upload

---
+++ intro_inputs.py
@@ -0,0 +1,7 @@
+favorite_city = input("What's your favorite city?\n")
+
+print(f"I agree, {favorite_city} would be on my list too")
+
+
+top_3 = input("What are your top 3 favorite food?\n")
+print(f"I'll keep it in mind when we go out to eat one day, {top_3}, noted")
