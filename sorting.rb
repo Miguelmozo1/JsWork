8a6f927a3722674b91062dbab849ac929595b606
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2023-11-24 21:24:46 -0800

    Add files via upload

---
+++ sorting.rb
@@ -0,0 +1,7 @@
+# introduced to rb's built in sorter. Sorts lowest to highest. Apperently by same method, it also sorts in alphabetical order
+# .sort!
+num_array = [1,34,23,54,11,25,90]
+puts(num_array.sort!)
+
+sports = ["hockey", "soccer", "football", "hungting"]
+puts(sports.sort!)
